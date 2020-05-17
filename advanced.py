6b28d74ca92e4ef0339d084b44f11db8b256d359
ramamanoj142000 <ramamanoj142000@yahoo.com>
2020-05-17 14:15:03 -0700

    adding new file

---
+++ advanced.py
@@ -0,0 +1,15 @@
+import time 
+
+def return_prime_number_under_param(max):
+    prime = []
+    return prime
+
+start = time.time()
+print(return_prime_number_under_param(1000))
+end = time.time()
+print (end-start)
+
+
+
+
+
